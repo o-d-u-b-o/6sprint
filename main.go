943c923f1c6df68a93c68cc5a5f025470d1190cb
o-d-u-b-o <odubo.ama@gmail.com>
2024-12-25 21:50:41 +0300

    first-iteration

--- main.go
+++ main.go
@@ -1,29 +1,128 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/Yandex-Practicum/final-project-encoding-go/encoding"
-	"github.com/Yandex-Practicum/final-project-encoding-go/utils"
+	"github.com/go-chi/chi/v5"
 )
+
+type Task struct {
+	ID          string   `json:"id"`          // ID задачи
+	Description string   `json:"description"` // Заголовок
+	Note        string   `json:"note"`        // Описание задачи
+	Application []string `json:"application"` // Приложения, которыми будете пользоваться
+}
 
 func Encode(data encoding.MyEncoder) error {
 	return data.Encoding()
 }
 
+var tasks = map[string]Task{
+	"1": {
+		ID:          "1",
+		Description: "Сделать финальное задание темы REST API",
+		Note:        "Если сегодня сделаю, то завтра будет свободный день. Ура!",
+		Application: []string{
+			"VS Code",
+			"Terminal",
+			"git",
+		},
+	},
+	"2": {
+		ID:          "2",
+		Description: "Протестировать финальное задание с помощью Postman",
+		Note:        "Лучше это делать в процессе разработки, каждый раз, когда запускаешь сервер и проверяешь хендлер",
+		Application: []string{
+			"VS Code",
+			"Terminal",
+			"git",
+			"Postman",
+		},
+	},
+}
+
+func getAllTasks(w http.ResponseWriter, r *http.Request) {
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func postTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tasks[task.ID] = task
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	ID := chi.URLParam(r, "id")
+
+	task, ok := tasks[ID]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusNotFound)
+		return
+	}
+
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	ID := chi.URLParam(r, "id")
+
+	if _, ok := tasks[ID]; !ok {
+		http.Error(w, "Задача не найдена", http.StatusNotFound)
+		return
+	}
+
+	delete(tasks, ID)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
-	utils.CreateJSONFile()
-	utils.CreateYAMLFile()
+	r := chi.NewRouter()
 
-	jsonData := encoding.JSONData{FileInput: "jsonInput.json", FileOutput: "yamlOutput.yml"}
-	err := Encode(&jsonData)
-	if err != nil {
-		fmt.Printf("ошибка при перекодировании данных из JSON в YAML: %s", err.Error())
+	r.Get("/tasks", getAllTasks)
+	r.Post("/tasks", postTask)
+	r.Get("/tasks/{id}", getTask)
+	r.Delete("/tasks/{id}", deleteTask)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
 	}
 
-	yamlData := encoding.YAMLData{FileInput: "yamlInput.yml", FileOutput: "jsonOutput.json"}
-	err = Encode(&yamlData)
-	if err != nil {
-		fmt.Printf("ошибка при перекодировании данных из YAML в JSON: %s", err.Error())
-	}
+	fmt.Println("Сервер запущен")
 }
